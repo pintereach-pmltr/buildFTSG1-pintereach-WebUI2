8b48e5a7fbab7658423576ded5ff1832993310f6
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-05-20 18:27:30 -0400

    added event listener on title

--- js/index.js
+++ js/index.js
@@ -17,3 +17,15 @@ class Dropdown {
 }
 
 let dropdown = document.querySelectorAll('.dropdown-menu').forEach(dropdown => new Dropdown(dropdown));
+
+let title = document.querySelector('.logo h1');
+
+title.addEventListener('mouseenter', () => {
+    title.textContent = 'The Ultimate Article Organizer';
+    title.style.color = '#EDF2F4'
+})
+
+title.addEventListener('mouseleave', () => {
+    title.textContent = 'Pintereach';
+    title.style.color = '#2B2D42'
+})
