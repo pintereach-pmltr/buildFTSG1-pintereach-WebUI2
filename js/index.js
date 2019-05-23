5c49f9927900e4c705e8ea794d855a400f00142b
Tyler Thompson <tylerthompson@Tylers-MacBook-Pro.local>
2019-05-23 17:53:25 -0400

    added second pallete

--- js/index.js
+++ js/index.js
@@ -22,14 +22,10 @@ let title = document.querySelector('.logo h1');
 
 title.addEventListener('mouseenter', () => {
     title.textContent = 'The Ultimate Article Organizer';
-    title.style.color = '#EDF2F4'
-    document.querySelector('.fa-bookmark').style.color = '#EDF2F4'
 })
 
 title.addEventListener('mouseleave', () => {
     title.textContent = 'Pintereach';
-    title.style.color = '#2B2D42'
-    document.querySelector('.fa-bookmark').style.color = '#2B2D42'
 })
 
 class Member {
